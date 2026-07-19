1067754b0e54267cebfb5637b4706f068d85d3d5
acrouch12 <ashtoncrouch12@gmail.com>
2026-07-19 10:42:48 -0400

    Add files via upload

---
+++ test-deploy/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HaulBoard from "./haulboard.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HaulBoard />
+  </React.StrictMode>
+);
